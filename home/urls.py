05dcdda85e9dc72cd727509b5a7717b46f1e99aa
mgymrek <mgymrek@mit.edu>
2013-12-01 17:18:19 -0500

    added back resources in urls

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'home.views.home', name='home'),
     url(r'^publications/', include('publications.urls')),
+    url(r'^resources/', include('resources.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
